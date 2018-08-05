948266f6c28ebf1a871367ac3863f4c18d12b3fe
Hwang In Seo <sjsjsj1246@gmail.com>
2018-08-06 04:11:21 +0900

    0806-1

--- javaFX_tutorial/src/ch/makery/address/MainApp.java
+++ javaFX_tutorial/src/ch/makery/address/MainApp.java
@@ -2,7 +2,11 @@ package ch.makery.address;
 
 import java.io.IOException;
 
+import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -13,7 +17,23 @@ public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
 
+    /**
+     * 생성자
+     */
+    public MainApp() {
+        // 샘플 데이터를 추가한다
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -55,10 +75,22 @@ public class MainApp extends Application {
 
             // 연락처 요약을 상위 레이아웃 가운데로 설정한다.
             rootLayout.setCenter(personOverview);
+            
+         // 메인 애플리케이션이 컨트롤러를 이용할 수 있게 한다.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    /**
+     * 연락처에 대한 observable 리스트를 반환한다.
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+    
 
     /**
      * 메인 스테이지를 반환한다.
