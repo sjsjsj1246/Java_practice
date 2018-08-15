adaf1577c499d8e49c724083439acd6a85a5d047
Hwang In Seo <sjsjsj1246@gmail.com>
2018-08-16 06:01:12 +0900

    0816

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -5,21 +5,46 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
 public class Main extends Application {
 
+	private Stage primaryStage;
+	private VBox rootLayout;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("list.fxml"));
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("test");
-		primaryStage.setResizable(false);
-		primaryStage.show();
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("test");
+		
+		initRootLayout();
+		showPersonOverview();
+	}
+	
+	public void initRootLayout() {
+		try {
+			rootLayout = FXMLLoader.load(getClass().getResource("root.fxml"));
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public void showPersonOverview() {
+		try {
+			VBox list = FXMLLoader.load(getClass().getResource("list.fxml"));
+			
+			rootLayout.getChildren().add(list);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
