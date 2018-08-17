846156e92bd0aa71c3b8013ac636aeb1ed14f843
Hwang In Seo <sjsjsj1246@gmail.com>
2018-08-18 01:06:19 +0900

    0817

--- EC_Advance/src/apllication/MainApp.java
+++ EC_Advance/src/apllication/MainApp.java
@@ -1,15 +1,59 @@
 package apllication;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+	private Stage primaryStage;
+	private VBox rootLayout;
+	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("test");
+		this.primaryStage.setResizable(false);
 		
+		initRootLayout();
+		showRepoTapLayout();
 	}
 
+	 /**
+     * 상위 레이아웃을 초기화한다.
+     */
+	public void initRootLayout() {
+		try {
+			// fxml 파일에서 상위 레이아웃을 가져온다.
+			FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (VBox) loader.load();
+			
+			// 상위 레이아웃을 포함하는 scene을 보여준다.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void showRepoTapLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RepoTapLayout.fxml"));
+            VBox RepoTapLayout = (VBox) loader.load();
+			
+			rootLayout.getChildren().add(RepoTapLayout);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
